b113ce6daa4e36ec933b523e6990b59260138393
Stephen Nguyen <nguyenhstephen@gmail.com>
2013-11-04 11:13:10 -0800

    added support for aws-s3

--- iron_doc/iron-doc-worker.rb
+++ iron_doc/iron-doc-worker.rb
@@ -3,8 +3,6 @@ require "httparty"
   # slightly unsure if Pandoc Ruby works correctly
   # PandocRuby.bin_path = "/usr/local/bin"
 
-
-
 # read contents of file save into response variable
 response = HTTParty.get(params["file_location"])
 # setup conversion
@@ -12,7 +10,7 @@ response = HTTParty.get(params["file_location"])
 # convert the sucker
 @doc =  @doc.convert
 # open write to new file
-file = File.open(params["file_name"], 'w') {|f| f.write(@doc)}
+File.open(params["file_name"], 'w') {|f| f.write(@doc)}
 
 if params["iron_cache"]
   require "iron_cache"
@@ -24,13 +22,12 @@ if params["iron_cache"]
   item = @cache.get("test")
 end
 
-# if params["s3"]
-#   require 'aws/s3'
-#   client = AWS::S3::Base.establish_connection!(
-#     :access_key_id     => params["s3_access_key_id"],
-#     :secret_access_key => params["s3_secret_access_key"]
-#     )
-#   binding.pry
-#   AWS::S3::Service.bucket.find(params["s3_bucket"])
-#   AWS::S3::S3Object.store(params["file_name"], file ,params["s3_bucket"] )
-# end
+if params["s3"]
+  require 'aws/s3'
+  client = AWS::S3::Base.establish_connection!(
+    :access_key_id     => params["s3_access_key_id"],
+    :secret_access_key => params["s3_secret_access_key"]
+)
+  bucket AWS::S3::Service.buckets.find {|b| b.name == params["s3_bucket"]}
+  AWS::S3::S3Object.store(params["file_name"], File.read(params["file_name"]) ,params["s3_bucket"] )
+end
